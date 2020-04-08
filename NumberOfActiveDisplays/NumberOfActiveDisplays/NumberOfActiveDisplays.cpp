ef2b8f5cf13c616897502e3b81e35d6b1255a5ed
Daniel Henry <daniel.henry@epiforge.com>
2020-04-08 16:54:15 -0400

    more detailed errors

--- NumberOfActiveDisplays/NumberOfActiveDisplays/NumberOfActiveDisplays.cpp
+++ NumberOfActiveDisplays/NumberOfActiveDisplays/NumberOfActiveDisplays.cpp
@@ -7,11 +7,25 @@ int main()
 {
     UINT32 pathCount, modeCount;
     LONG getDisplayConfigBufferSizesResult = GetDisplayConfigBufferSizes(QDC_ONLY_ACTIVE_PATHS, &pathCount, &modeCount);
-    if (getDisplayConfigBufferSizesResult != 0)
+    switch (getDisplayConfigBufferSizesResult)
     {
+    case ERROR_SUCCESS:
+        cout << pathCount;
+        return 0;
+    case ERROR_INVALID_PARAMETER:
+        cerr << "GetDisplayConfigBufferSizes says the combination of parameters and flags I've specified is invalid";
+        return 2;
+    case ERROR_NOT_SUPPORTED:
+        cerr << "GetDisplayConfigBufferSizes says the system is not running a graphics driver that was written according to the Windows Display Driver Model (WDDM), which it requires";
+        return 3;
+    case ERROR_ACCESS_DENIED:
+        cerr << "GetDisplayConfigBufferSizes says the I do not have access to the console session";
+        return 4;
+    case ERROR_GEN_FAILURE:
+        cerr << "GetDisplayConfigBufferSizes says an unspecified error occurred (which is immensely helpful)";
+        return 5;
+    default:
         cerr << "GetDisplayConfigBufferSizes returned unsuccessful result: " << getDisplayConfigBufferSizesResult;
         return 1;
     }
-    cout << pathCount;
-    return 0;
 }
